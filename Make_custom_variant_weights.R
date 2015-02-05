5e5aa619e1e46812a2c65e246370305999e49b60
ttimbers <tiffany.timbers@gmail.com>
2015-02-04 16:35:11 -0800

    Added more files to ignore. Also vetted an R script to make custom variable weights for SKAT analysis. Would still like to build in some flexibility to be able to change the weights when I call the function

---
+++ Make_custom_variant_weights.R
@@ -0,0 +1,106 @@
+##Make weights file for SKAT
+
+custom_weights <- function(filename) {
+
+  ##remove header from .vcf file
+  system(paste("grep -v '^#' ", filename, " > MMPdyf_non-syn_coding_no_header.txt"))
+
+  ##grep indels and snv's into two separate groups and create text files with this data
+  system("grep -h 'INDEL' MMPdyf_non-syn_coding_no_header.txt > MMPdyf_indels.txt")
+  system("grep -v 'INDEL' MMPdyf_non-syn_coding_no_header.txt > MMPdyf_snvs.txt")
+
+  ##import text files created above
+  variants.indels <- read.table("MMPdyf_indels.txt")
+  variants.snvs <- read.table("MMPdyf_snvs.txt")
+
+  ##Get variant names, amino acid change, indel label and reading frame change info for indels
+  ##get indel variant names
+  variants.indels.names <- variants.indels$V3
+  vars.indel <- as.character(variants.indels.names)
+
+  ##get indel amino acid change
+  library(stringr)
+  aac_tag.indel  <- "AAC=[a-zA-Z0-9*>-_]{1,}"
+  amino.acid.change.indel  <- str_extract(variants.indels$V8, aac_tag.indel)
+  amino.acid.change.indel  <- sub("AAC=", "", amino.acid.change.indel)
+  aacs.indel  <- as.character(amino.acid.change.indel)
+
+  ##get indel label from indels
+  indel.label.indels <- rep("yes", dim(variants.indels)[1])
+
+  ##get reading frame change
+  rfc_tag.indel  <- "RFC=[a-z]{1,}"
+  reading.frame.change.indel  <- str_extract(variants.indels$V8, rfc_tag.indel)
+  reading.frame.change.indel  <- sub("RFC=", "", reading.frame.change.indel)
+  rfcs.indel <- as.character(reading.frame.change.indel)
+
+  ##Get variant names and reading frame change info for snvs
+  ##get snv variant names
+  variants.snvs.names <- variants.snvs$V3
+  vars.snvs <- as.character(variants.snvs.names)
+
+  ##get snv amino acid change
+  aac_tag.snv  <- "AAC=[a-zA-Z0-9*>-]{1,}"
+  amino.acid.change.snv  <- str_extract(variants.snvs$V8, aac_tag.snv)
+  amino.acid.change.snv  <- sub("AAC=", "", amino.acid.change.snv)
+  aacs.snv  <- as.character(amino.acid.change.snv)
+
+  ##get indel label from snvs
+  indel.label.snvs <- rep("no", dim(variants.snvs)[1])
+
+  ##get reading frame change from snvs
+  reading.frame.change.snv  <- rep("no", dim(variants.snvs)[1])
+  rfcs.snv <- as.character(reading.frame.change.snv)
+
+  ##concatenate indel variants with snv variants
+  vars <- c(vars.indel, vars.snvs)
+  aacs  <- c(aacs.indel, aacs.snv)
+  indel.labels <- c(indel.label.indels, indel.label.snvs)
+  rfcs <- c(rfcs.indel, rfcs.snv)
+  coding.changes  <- data.frame(cbind(vars, aacs, indel.labels, rfcs))
+
+  ##get inframe deletions (indel.lables == true and rfcs == no) and assign a weight of 0.5
+  indel.inframe <- subset(coding.changes, coding.changes$indel.labels == "yes" & coding.changes$rfcs == "no")
+  weight <- rep(x=0.5, dim(indel.inframe)[1])
+  indel.inframe <- data.frame(cbind(indel.inframe, weight))
+
+  ##frameshift causing indels and assign a weight of 1
+  indel.frameshift <- subset(coding.changes, coding.changes$rfcs == "yes")
+  weight <- rep(x=1, dim(indel.frameshift)[1])
+  indel.frameshift <- data.frame(cbind(indel.frameshift, weight))
+
+  ##nonsense mutations and assign a weight of 1
+  index.nonsense <- grep("[-][>][*]", coding.changes$aacs)
+  nonsense  <- coding.changes[index.nonsense,]
+  weight <- rep(x=1, dim(nonsense)[1])
+  nonsense <- data.frame(cbind(nonsense, weight))
+
+  ##readthrough mutations and assign a weight of 1
+  index.readthrough <- grep("[*][-][>]", coding.changes$aacs)
+  readthrough  <- coding.changes[index.readthrough,]
+  weight <- rep(x=1, dim(readthrough)[1])
+  readthrough <- data.frame(cbind(readthrough, weight))
+
+  ##indices of missense mutations and assign a weight of 0.25
+  index.muts <- grep("[-][>]", coding.changes$aacs)
+  muts <- coding.changes[index.muts,]
+  index.missense <- grep("[*]", muts$aacs, invert=TRUE)
+  missense  <- muts[index.missense,]
+  weight <- rep(x=0.25, dim(missense)[1])
+  missense <- data.frame(cbind(missense, weight))
+
+  ##indices of splicing mutations and assign a weight of 1
+  splicing.defect <- subset(coding.changes, coding.changes$aacs == "NA")
+  weight <- rep(x=1, dim(splicing.defect)[1])
+  splicing.defect <- data.frame(cbind(splicing.defect, weight))
+
+  ##combine all mutations
+  variant.weights <- rbind(splicing.defect, missense, readthrough, nonsense, indel.frameshift, indel.inframe)
+  vars.weights <- as.character(variant.weights$vars)
+  weight.weights <- as.character(variant.weights$weight)
+  variant.weights.file <- data.frame(cbind(vars.weights, weight.weights))
+  return(variant.weights.file)
+}
+
+variant.weights <- custom_weights("MMPdyf_non-syn_coding.vcf")
+write.table(variant.weights, "MMP_SNP_WeightFile.txt", sep="\t", row.names=FALSE, col.names=FALSE, quote=FALSE, append=FALSE)
