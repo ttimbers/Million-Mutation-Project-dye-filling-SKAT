e70e58993225b40ed463f3a0d33fa6b8f90956e1
ttimbers <tiffany.timbers@gmail.com>
2015-06-19 15:22:05 -0700

    Removed the scripts from the home directory and ignored the other directory

--- Remove_vcf_header.R
+++
@@ -1,7 +0,0 @@
-remove.vcf_header <- function(filename) {
-##remove header from .vcf file
-filename.out <- paste(substr(filename, 1, nchar(filename)-4), "_no_header.txt", sep="")
-system(paste("grep -v '^#' ", filename, " > ", filename.out, sep=""))
-}
-
-remove.vcf_header("MMP_non-syn_coding.vcf")
