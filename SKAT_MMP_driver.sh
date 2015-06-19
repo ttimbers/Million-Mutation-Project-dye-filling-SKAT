80da464f2b6d803caa703d449bf0f8d7dd8dc957
ttimbers <tiffany.timbers@gmail.com>
2015-06-19 15:36:22 -0700

    renamed:    bin/SKAT_MMP_dyf_pipeline.sh -> SKAT_MMP_driver.sh

--- SKAT_MMP_driver.sh
+++ SKAT_MMP_driver.sh
@@ -9,7 +9,7 @@
 ## from wild-type (Fisher's exact test) and contain 1 if strain's phenotype diverges 
 ## significantly from wild-type, and 0 if it does not. A 5% FDR (Benjamini-Hochberg 
 ## procedure) is used to adjust for multiple comparisons.
-Rscript Assign_dichotomous_phenotype.R
+Rscript bin/Assign_dichotomous_phenotype.R
 
 ## makes a file called phenotypes.txt from a file which lists strains in the first column 
 ## and phenotypes in all subsequent columns. Note input file must have a header listing
@@ -17,36 +17,21 @@ Rscript Assign_dichotomous_phenotype.R
 ## Script Arguments:
 ##    1. the name of the input file containing the phenotypes
 ##    2. the name of the output file
-Rscript Make_list_phenotype_file.R dyf_phenotypes_dichotomous.txt phenotypes.txt
+Rscript bin/Make_list_phenotype_file.R data/dyf_phenotypes_dichotomous.txt data/phenotypes.txt
 
 
 ## make list_VCstrains_vcf.txt file for strains assayed by extracting the first column 
 ## from a tab delimited text file to grab the strains from someone's tab delimited 
 ## phenotypes file:
-awk '{print $1}' dyf_phenotypes_dichotomous.txt | grep -h "^VC*" > vcf_files/list_VCstrains_vcf.txt
+awk '{print $1}' data/dyf_phenotypes_dichotomous.txt | grep -h "^VC*" > data/list_VCstrains_vcf.txt
 
-## Create one merged .vcf file for all strains that were assayed (listed in 
+## Create one merged .vcf file for only the strains that were assayed (listed in 
 ## a file called list_VCstrains_vcf.txt
-##
-## note: this .pl script also requires the use of a text file called 
-## gknames.txt which contains all variant names of the MMP library, as 
-## well as the VC*.combined.vcf files to combine into the merged file.
-## These all should be in the same directory
-cd vcf_files
-./create_gwas_vcf_MMP.pl > ../MMP.vcf
-cd ..
-
-## create a merged .vcf file with only variants which affect coding regions
-grep -v "GRANT=0" MMP.vcf > MMP_non-syn.vcf
-grep -h "^#" MMP_non-syn.vcf > MMP_non-syn_coding.vcf
-grep -h "coding" MMP_non-syn.vcf >> MMP_non-syn_coding_temp.vcf
-grep -h -v "Noncoding" MMP_non-syn_coding_temp.vcf >> MMP_non-syn_coding.vcf
-grep -h "GF=intron_splicing" MMP_non-syn.vcf >> MMP_non-syn_coding.vcf
-rm MMP.vcf
-rm MMP_non-syn.vcf
+## -protein argument creates a merged .vcf file with only variants which affect coding regions
+gzcat data/MMP.vcf.gz | perl bin/filter_MMP_variants.pl -input - -output data/filteredMMP.vcf -strain data/list_VCstrains_vcf.txt -protein
 
 ## make a version of the merged .vcf file without the header
-grep -v '^#' MMP_non-syn_coding.vcf > MMP_non-syn_coding.vcf
+grep -v '^#' data/filteredMMP.vcf > data/MMP_non-syn_coding.vcf
 
 ## Make custom weights for each variant based off of the type of mutation. 
 ## Script Arguments:
@@ -55,16 +40,7 @@ grep -v '^#' MMP_non-syn_coding.vcf > MMP_non-syn_coding.vcf
 ## 3: weight to assign inframe deletions
 ## 4: weight to assign missense mutations
 ## 5: name of file to save variant weights to 
-Rscript Make_custom_variant_weights.R MMP_non-syn_coding.vcf 1 0.75 0.25 MMP_SNP_WeightFile.txt
+Rscript Make_custom_variant_weights.R data/MMP_non-syn_coding.vcf 1 0.75 0.25 MMP_SNP_WeightFile.txt
 
 ## Do SKAT analysis
-## Rscript do_SKAT.R MMP_non-syn_coding.vcf MMP_non-syn_coding_SSID.txt phenotypes.txt dyf_phenotypes_dichotomous.txt
-
-## Make snp set ID file (SSID) to run SKAT analysis
-Rscript Make_SSID_file.R
-
-## Make plink files from merged .vcf file to run SKAT analysis
-Rscript Make_plink_files.R
-
-## Do SKAT analysis
-Rscript SKAT_logistic_strain.R
+Rscript do_SKAT.R data/MMP_non-syn_coding.vcf data/MMP_non-syn_coding_SSID.txt data/phenotypes.txt data/dyf_phenotypes_dichotomous.txt
