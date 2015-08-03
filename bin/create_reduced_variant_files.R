3b169605eb32709b4b8d7af8bd85ae3544ff5867
ttimbers <tiffany.timbers@gmail.com>
2015-08-03 12:00:42 -0700

    Wrote a script to make a smaller SSID file and vcf file for only the variants that have >= some minimum variant number specified by the user

---
+++ bin/create_reduced_variant_files.R
@@ -0,0 +1,44 @@
+## Tiffany Timbers
+## August 3, 2015
+##
+## Takes a vcf file, a SNP set ID file and the minimum number of variants you want to do SKAT for to create a vcf file and a a SNP set ID file with only those variants.
+
+main <- function() {
+  ## command line arguements
+  args <- commandArgs(trailingOnly = TRUE)
+  vcf_file <- args[1]
+  SSID_file <- args[2]
+  min_num_variants <- args[3]
+  vcf_output_file <- args[4]
+  SSID_output_file <- args[5]
+  
+
+  
+  ## load files
+  SSID <- read.table(SSID_file, header=FALSE)
+  vcf <- read.table(vcf_file, header=FALSE)
+  
+  ## make a list of the variants to keep (i.e. count the number of occurrences of each gene)
+  SSID_count <- count(SSID[,1])
+  
+  ## make SSID file with only those variants (i.e. reduce the SSID list to only
+  ## those >= min_num_variants)
+  SSID_count_greater_or_equal_to_min <- SSID[which(SSID_count[2] >= min_num_variants),]
+  
+  ## save SSID file
+  write.table(SSID_count_greater_or_equal_to_min, file = SNP_location_output_file, row.names = FALSE, col.names = FALSE, quote = FALSE)
+  
+  ## make a .vcf file with only those variants
+  require(dplyr)
+  variant_list <- data.frame(SSID_count_greater_or_equal_to_min[,2])
+  colnames(variant_list) <- ("V3")
+  var_table <- tbl_df(variant_list)
+  vcf_table <- tbl_df(vcf)
+  vcf_count_greater_or_equal_to_min <- inner_join(vcf_table, var_table)
+  
+  ## save .vcf file
+  write.table(vcf_count_greater_or_equal_to_min, file = vcf_output_file, row.names = FALSE, col.names = FALSE, quote = FALSE)
+  
+}
+
+main()
