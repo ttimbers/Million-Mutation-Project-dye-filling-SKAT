b586e7945b3116bcee32afe9d1c6c9119421fde6
ttimbers <tiffany.timbers@gmail.com>
2015-08-03 10:19:22 -0700

    modified create_haplotype_matrix.R to output haplotype matrix without row or column names as well as a SNPlocation vector

--- bin/create_haplotype_matrix.R
+++ bin/create_haplotype_matrix.R
@@ -8,7 +8,8 @@
 ##
 ## Arguements:  (1) path to vcf file
 ##              (2) column number of the genotype for the first individual
-##              (3) path and name of file to save to
+##              (3) path and name of file to save Haplotype matrix to
+##              (4) path and name of file to save SNP location vector to
 ##
 ## Dependencies:  (1) R and R libraries stringr and plyr
 ##                (2) Bash Shell
@@ -17,21 +18,23 @@ main <- function() {
   args <- commandArgs(trailingOnly = TRUE)
   vcf_file <- args[1]
   first_genotype_column <- args[2]
-  output_file <- args[3]
+  haplotype_output_file <- args[3]
+  SNP_location_output_file <- args[4]
+  
   
   ## load libraries
   require(stringr)
   require(plyr)
   
-  ## reduce matrix to only variant names and variant calls
+  
+  ## Make haplotype matrix
   
   ## make a variable to hold the filename I will give to a rectangular version of the vcf 
   ## file (withoug column 1)
   vcf_df_no_header_name <- paste(vcf_file, ".no_header", sep="")
   
   ## make a rectangular version of the vcf file (withoug column 1)
-  system(paste("grep -E -v '^##' ", vcf_file, " | cut -d ' ' -f 2,3,", first_genotype_column, 
-               "- > ", vcf_df_no_header_name, sep = ''))
+  system(paste("grep -E -v '^##' ", vcf_file, " | cut -d ' ' -f 2,3,", first_genotype_column, "- > ", vcf_df_no_header_name, sep = ''))
   
   ## read rectangular version into R
   vcf_df_no_header <- read.table(vcf_df_no_header_name, header=TRUE)
@@ -44,16 +47,21 @@ main <- function() {
   temp_df[temp_df=="0/0"] <- 0
   temp_df[temp_df=="1/1"] <- 1
   
-  ## make column #1 the row names
-  temp_df2 <- temp_df[,-1]
-  rownames(temp_df2) <- temp_df[,1]
+  ## make a dataframe of the 0 and 1 values (i.e. ID column)
+  haploytpe_matrix_t <- temp_df[,-1]
   
-  # transpose dataframe
-  haplotype_matrix <- as.data.frame(t(as.matrix(temp_df2[-1])))
+  ## transpose dataframe
+  haplotype_matrix <- as.data.frame(t(as.matrix(haploytpe_matrix_t)))
   
-  # save to output file
-  write.table(haplotype_matrix, file = output_file, row.names = TRUE, col.names = TRUE, 
-              quote = FALSE)
+  ## save to haplotype matrix to output file
+  write.table(haplotype_matrix, file = haplotype_output_file, row.names = FALSE, col.names = FALSE, quote = FALSE)
+  
+  
+  ## Make SNP location vector
+  SNP_location_vector <- vcf_df_no_header[,1]
+  
+  ## save to SNP location vector to output file
+  write.table(SNP_location_vector, file = SNP_location_output_file, row.names = FALSE, col.names = FALSE, quote = FALSE)
 }
 
 main()
