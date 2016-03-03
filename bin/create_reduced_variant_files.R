01432af5f313ac2df8e9bd5ecf1fc195d2ad38fc
ttimbers <tiffany.timbers@gmail.com>
2016-03-03 13:46:44 -0800

    Fixed bug in create_reduced_variant_files.R
    Discovered that the bug that was in this file was due to the command line argument of a number be automatically converted to a string. Of course this does not happen in RStudio, and so that is why I kept getting different numbers when trying to subset with  "7" instead of 7.

--- bin/create_reduced_variant_files.R
+++ bin/create_reduced_variant_files.R
@@ -3,12 +3,6 @@
 ##
 ## Takes a vcf file, a SNP set ID file and the minimum number of variants you want to do SKAT for to create a vcf file and a a SNP set ID file with only those variants.
 
-#vcf_file <- "data/MMPcoding.vcf"
-#SSID_file <- "data/MMPcoding.SSID"
-#min_num_variants <- 7
-#vcf_output_file <- "data/MMPfiltered.vcf"
-#SSID_output_file <- "data/MMPfiltered.SSID"
-
 main <- function() {
   ## load libraries
   require(dplyr)
@@ -17,7 +11,8 @@ main <- function() {
   args <- commandArgs(trailingOnly = TRUE)
   vcf_file <- args[1]
   SSID_file <- args[2]
-  min_num_variants <- args[3]
+  # command line arg is a string, so need to be converted to a number
+  min_num_variants <- as.numeric(args[3])
   vcf_output_file <- args[4]
   SSID_output_file <- args[5]
 
@@ -31,17 +26,12 @@ main <- function() {
 
   ## make SSID file with only those variants (i.e. reduce the SSID list to only
   ## those >= min_num_variants)
-  #genes_to_include <- data.frame(SSID_count[which(SSID_count[,2] >= min_num_variants),1])
   genes_to_include <- data.frame(SSID_count[SSID_count$freq >= min_num_variants, 1])
-  #df[df$aged <= df$laclen, ]
   colnames(genes_to_include) <- ("gene")
   gene_table <- tbl_df(genes_to_include)
   SSID_table <- tbl_df(SSID)
   SSID_count_greater_or_equal_to_min <- semi_join(SSID_table, gene_table)
   SSID_to_print <- data.frame(lapply(SSID_count_greater_or_equal_to_min, as.character), stringsAsFactors=FALSE)
-
-  print("SSID_tp_print dimensions are: ")
-  print(dim(SSID_to_print))
 
   ## save SSID file
   write.table(SSID_to_print, file = SSID_output_file, row.names = FALSE, col.names = FALSE, quote = FALSE)
@@ -53,8 +43,6 @@ main <- function() {
   vcf_table <- tbl_df(vcf)
   vcf_count_greater_or_equal_to_min <- semi_join(vcf_table, var_table)
   vcf_to_print <- data.frame(lapply(vcf_count_greater_or_equal_to_min, as.character), stringsAsFactors=FALSE)
-  print("vcf_tp_print dimensions are: ")
-  print(dim(vcf_to_print))
 
   ## save .vcf file
   system(paste("grep -E '^#' ", vcf_file, " > ", vcf_output_file, sep=""))
