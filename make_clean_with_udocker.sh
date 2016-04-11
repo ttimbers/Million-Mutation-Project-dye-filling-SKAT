13f7b2cb2f23c77d02fd2b61ec88eecd66c43843
ttimbers <tiffany.timbers@gmail.com>
2016-04-11 12:09:59 -0700

    fixed documentation for usage for make clean shell script for udocker

--- make_clean_with_udocker.sh
+++ make_clean_with_udocker.sh
@@ -7,10 +7,10 @@
 # Arguments: 1. path to Million-Mutation-Project-dye-filling-SKAT directory inside container
 #
 # How to run on Compute Canada's guillimin server:
-# udocker run --rm ttimbers/mmp-dyf-skat bash /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT
+# udocker run --rm ttimbers/mmp-dyf-skat bash /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT/make_clean_with_udocker.sh /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT
 #
 # Example of how I ran it on my local machine:
-# docker run --rm -v /Users/tiffanytimbers/Documents/Post-Doc/Manuscripts/MMP_dyf_screen/code/Million-Mutation-Project-dye-filling-SKAT:/home/Million-Mutation-Project-dye-filling-SKAT ttimbers/mmp-dyf-skat bash /home/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh /home/Million-Mutation-Project-dye-filling-SKAT
+# docker run --rm -v /Users/tiffanytimbers/Documents/Post-Doc/Manuscripts/MMP_dyf_screen/code/Million-Mutation-Project-dye-filling-SKAT:/home/Million-Mutation-Project-dye-filling-SKAT ttimbers/mmp-dyf-skat bash /home/Million-Mutation-Project-dye-filling-SKAT/make_clean_with_udocker.sh /home/Million-Mutation-Project-dye-filling-SKAT
 
 # export path to plink to $PATH
 export PATH="/usr/bin/plink:$PATH"
