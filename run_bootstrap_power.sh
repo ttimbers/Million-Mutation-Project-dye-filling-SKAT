24ef33d0f620771c16ebcfbf98d86507d4026238
ttimbers <tiffany.timbers@gmail.com>
2016-05-08 12:43:23 -0700

    changes

--- run_bootstrap_power.sh
+++ run_bootstrap_power.sh
@@ -2,8 +2,8 @@
 #PBS -l walltime=03:00:00
 #PBS -l nodes=1:ppn=1
 
-# load docker image
-udocker load docker_images/mmp-dyf-skat.tar
+# load docker image is currently not working??
+#udocker load --input docker_images/mmp-dyf-skat.tar
 
 # run Makefile to make all targets for Million-Mutation-Project-dye-filling-SKAT in ttimbers/mmp-dyf-skat
-udocker run --rm -w /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT ttimbers/mmp-dyf-skat bash /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT/bootstrap_power.sh 200 
+udocker run --rm -w /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT ttimbers/mmp-dyf-skat bash /home/ttimbers/projects/Million-Mutation-Project-dye-filling-SKAT/bin/bootstrap_power.sh 200
