8a89027f8b59c088c3e06fb1a530aaeaeee2caa6
ttimbers <tiffany.timbers@gmail.com>
2016-05-15 12:28:47 -0700

    Changed power analysis output
    Decided to use the Docker container ID to create a directory and put the output of each power analysis in its own directory, instead of modifiing file names.

--- bin/bootstrap_power.sh
+++ bin/bootstrap_power.sh
@@ -17,24 +17,24 @@ echo $1 >> data/stdout.txt
 mkdir data/$cID
 
 ## Create list of randomly sampled strains (without replacement) & phenotype data from data/phenotype_amphid_dyf_dichotomous.csv
-Rscript bin/create_random_samples.R data/phenotype_amphid_dyf_dichotomous.csv \t TRUE 1 $1 data/temp_phenotype_amphid_dyf_dichotomous.csv.$cID
+Rscript bin/create_random_samples.R data/phenotype_amphid_dyf_dichotomous.csv \t TRUE 1 $1 data/$cID/temp_phenotype_amphid_dyf_dichotomous.csv
 
 ## Create list of randomly selected strains from temp_phenotype_amphid_dyf.csv
-awk '{print $1}' data/temp_phenotype_amphid_dyf_dichotomous.csv.$cID | grep -h "^VC*" > data/temp_list_VCstrains_vcf.txt.$cID
+awk '{print $1}' data/$cID/temp_phenotype_amphid_dyf_dichotomous.csv | grep -h "^VC*" > data/$cID/temp_list_VCstrains_vcf.tsv
 
 ## Create a vcf file from these random selected strains, only variants from data/MMPfiltered.vcf
-gunzip -c data/MMP.vcf.gz | perl bin/filter_MMP_variants.pl -input - -output data/temp_MMPcoding.vcf.$cID -strain data/temp_list_VCstrains_vcf.txt.$cID -protein
+gunzip -c data/MMP.vcf.gz | perl bin/filter_MMP_variants.pl -input - -output data/$cID/temp_MMPcoding.vcf -strain data/$cID/temp_list_VCstrains_vcf.tsv -protein
 
 ## Create SSID file for SKAT analysis
-Rscript bin/Make_SSID_file.R data/temp_MMPcoding.vcf.$cID data/temp_MMPcoding.SSID.$cID
+Rscript bin/Make_SSID_file.R data/$cID/temp_MMPcoding.vcf data/$cID/temp_MMPcoding.SSID
 
 ## Create a filtered SSID file and vcf file for only variants from those genes which have
 ## a specified minimum number of alleles (we chose 7)
-Rscript bin/create_reduced_variant_files.R data/temp_MMPcoding.vcf.$cID data/temp_MMPcoding.SSID.$cID 7 data/temp_MMPfiltered.vcf.$cID data/temp_MMPfiltered.SSID.$cID
+Rscript bin/create_reduced_variant_files.R data/$cID/temp_MMPcoding.vcf data/$cID/temp_MMPcoding.SSID 7 data/$cID/temp_MMPfiltered.vcf data/$cID/temp_MMPfiltered.SSID
 
 ## Create binary plink files from the vcf file
-if [ ! -d "data/$cID" ]; then mkdir data/$cID; fi
-plink --vcf data/temp_MMPfiltered.vcf.$cID --allow-extra-chr --no-fid --no-parents --no-sex --no-pheno --out data/power/temp_MMPfiltered
+#if [ ! -d "data/$cID" ]; then mkdir data/$cID; fi
+plink --vcf data/$cID/temp_MMPfiltered.vcf --allow-extra-chr --no-fid --no-parents --no-sex --no-pheno --out data/$cID/temp_MMPfiltered
 
 ## Perform SKAT analysis
-Rscript bin/do_SKAT_no_weights.R data/$cID/temp_MMPfiltered.fam data/temp_phenotype_amphid_dyf_dichotomous.csv.$cID data/$cID data/temp_MMPfiltered.SSID.$cID
+Rscript bin/do_SKAT_no_weights.R data/$cID/temp_MMPfiltered.fam data/$cID/temp_phenotype_amphid_dyf_dichotomous.csv data/$cID data/$cID/temp_MMPfiltered.SSID
