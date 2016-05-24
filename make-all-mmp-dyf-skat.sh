5fed2039489d64493ead73ec8c6e82e882b9c45e
ttimbers <tiffany.timbers@gmail.com>
2016-05-24 06:00:55 -0700

    gave absolute path to udocker load command

--- make-all-mmp-dyf-skat.sh
+++ make-all-mmp-dyf-skat.sh
@@ -3,7 +3,7 @@
 #PBS -l nodes=1:ppn=8
 
 # load docker image
-udocker load docker_images/mmp-dyf-skat.tar
+udocker load /home/ttimbers/docker_images/mmp-dyf-skat.tar
 
 # run Makefile to make all targets for Million-Mutation-Project-dye-filling-SKAT in ttimbers/mmp-dyf-skat
-udocker run --rm /home/ttimbers/mmp-dyf-skat bash /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh all /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT
+udocker run --rm ttimbers/mmp-dyf-skat bash /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh all /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT
