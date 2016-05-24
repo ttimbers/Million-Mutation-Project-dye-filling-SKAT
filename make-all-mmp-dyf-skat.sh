2e520d23ee70f44910459ea7753657dd5bf3a591
ttimbers <tiffany.timbers@gmail.com>
2016-05-24 06:03:48 -0700

    added --input flag to udocker command

--- make-all-mmp-dyf-skat.sh
+++ make-all-mmp-dyf-skat.sh
@@ -3,7 +3,7 @@
 #PBS -l nodes=1:ppn=8
 
 # load docker image
-udocker load /home/ttimbers/docker_images/mmp-dyf-skat.tar
+udocker load --input /home/ttimbers/docker_images/mmp-dyf-skat.tar
 
 # run Makefile to make all targets for Million-Mutation-Project-dye-filling-SKAT in ttimbers/mmp-dyf-skat
 udocker run --rm ttimbers/mmp-dyf-skat bash /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh all /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT
