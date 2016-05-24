1e0da512f4b9084702e7a8871bbe6e247a93f9f1
ttimbers <tiffany.timbers@gmail.com>
2016-05-24 12:24:50 -0700

    Changed container vol

--- make-all-mmp-dyf-skat.sh
+++ make-all-mmp-dyf-skat.sh
@@ -7,4 +7,4 @@ udocker load --input /home/ttimbers/docker_images/mmp-dyf-skat.tar
 
 # run Makefile to make all targets for Million-Mutation-Project-dye-filling-SKAT in ttimbers/mmp-dyf-skat
 #udocker run --rm -w /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT ttimbers/mmp-dyf-skat bash /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh all
-udocker run --rm -it -v /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT:/gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT -w /home/Million-Mutation-Project-dye-filling-SKAT ttimbers/mmp-dyf bash /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh all
+udocker run --rm -it -v /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT:/gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT -w /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT ttimbers/mmp-dyf bash /gs/project/qkh-103-aa/Million-Mutation-Project-dye-filling-SKAT/make_with_udocker.sh all
