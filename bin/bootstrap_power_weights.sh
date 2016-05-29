0b0d1ae7ca9c2fdaec445c03d86d5e88a013a189
ttimbers <tiffany.timbers@gmail.com>
2016-05-29 11:33:09 -0700

    Added script to do power analysis with weights

---
+++ bin/bootstrap_power_weights.sh
@@ -0,0 +1,54 @@
+# Shell script to iterate over for bootstrap power analysis
+# Tiffany Timbers
+# 2016-05-08
+
+# export path to plink to $PATH
+export PATH="/usr/bin/plink:$PATH"
+
+# get container ID
+cID=$(cat /proc/self/cgroup | grep "cpu:/" | sed 's/\([0-9]\):cpu:\/docker\///g')
+
+## testing that we can make directories with the jobID
+mkdir data/$cID
+
+## Create list of randomly sampled strains (without replacement) & phenotype data from data/phenotype_amphid_dyf_dichotomous.csv
+Rscript bin/create_random_samples.R data/phenotype_amphid_dyf_log_005.tsv \t TRUE 1 $1 data/$cID/temp_phenotype_amphid_dyf_log.tsv
+
+## Create list of randomly selected strains from temp_phenotype_amphid_dyf.csv
+awk '{print $1}' data/$cID/temp_phenotype_amphid_dyf_log.tsv | grep -h "^VC*" > data/$cID/temp_list_VCstrains_vcf.tsv
+
+## Create a vcf file from these random selected strains, only variants from data/MMPfiltered.vcf
+gunzip -c data/MMP.vcf.gz | perl bin/filter_MMP_variants.pl -input - -output data/$cID/temp_MMPcoding.vcf -strain data/$cID/temp_list_VCstrains_vcf.tsv -protein
+
+## Create SSID file for SKAT analysis
+Rscript bin/Make_SSID_file.R data/$cID/temp_MMPcoding.vcf data/$cID/temp_MMPcoding.SSID
+
+## Create a filtered SSID file and vcf file for only variants from those genes which have
+## a specified minimum number of alleles (we chose 7)
+Rscript bin/create_reduced_variant_files.R data/$cID/temp_MMPcoding.vcf data/$cID/temp_MMPcoding.SSID 7 data/$cID/temp_MMPfiltered.vcf data/$cID/temp_MMPfiltered.SSID
+
+## Create a custom weights for each variant based off of the type of mutation.
+## Script Arguments:
+## 1: name of .vcf file containing coding variants
+## 2: weight to assign "knockout" mutations
+## 3: weight to assign inframe deletions
+## 4: weight to assign missense mutations
+## 5: name of file to save variant weights to
+data/MMP_SNP_WeightFile.txt: bin/Make_custom_variant_weights.R data/MMPfiltered.vcf
+	Rscript bin/Make_custom_variant_weights.R data/MMPfiltered.vcf 1 0.75 0.25 data/MMP_SNP_WeightFile.txt
+
+## Create binary plink files from the vcf file
+#if [ ! -d "data/$cID" ]; then mkdir data/$cID; fi
+plink --vcf data/$cID/temp_MMPfiltered.vcf --allow-extra-chr --no-fid --no-parents --no-sex --no-pheno --out data/$cID/temp_MMPfiltered
+
+## Perform SKAT analysis
+Rscript bin/do_SKAT_weights.R data/$cID/temp_MMPfiltered.fam data/$cID/temp_phenotype_amphid_dyf_log.tsv data/$cID data/$cID/temp_MMPfiltered.SSID
+
+## add N to dataframe
+Rscript bin/add_column_to_tsv.R data/$cID/SKAT_weights.tsv $1 N data/$cID/SKAT_weights.tsv
+
+## add container ID to dataframe
+Rscript bin/add_column_to_tsv.R data/$cID/SKAT_weights.tsv $cID ID data/$cID/SKAT_weights.tsv
+
+## clean up unneccesary files
+rm data/$cID/temp* data/$cID/file*
--- bin/do_SKAT_weights.R
+++ bin/do_SKAT_weights.R
@@ -1,4 +1,4 @@
-## Tiffany Timbers, 2015
+## Tiffany Timbers, 2016
 ## Script to run SKAT analysis
 ##
 ## Inputs:
@@ -6,14 +6,9 @@
 ## 	2. path to phenotype file (two column file where first column is strain and second column is phenotype)
 ## 	3. path to PLINK files
 ## 	4. path to SSID file
-##	5. path to weights file
-##  6. postfix for output file
 ##
 ## Outputs:
-##	1. SKAT results file for analysis with no weights
-##	2. SKAT results file with FDR q values for analysis with no weights
-##	3. SKAT results file for analysis with weights
-## 	4. SKAT results file with FDR q values for analysis with weights
+##	1. SKAT results file with FDR q values for analysis with no weights
 
 main <- function() {
 
@@ -21,8 +16,7 @@ main <- function() {
   	ptm <- proc.time()
 
 	## load libraries
-	library(SKAT)
-  library(qqman)
+	require(SKAT)
 
 	## assign command line arguements
 	args <- commandArgs(trailingOnly = TRUE)
@@ -30,8 +24,7 @@ main <- function() {
 	path_to_phenotype.filename <- args[2]
 	path_to_plink_files <- args[3]
   SSID_file <- args[4]
-	weights.file <- args[5]
-	postfix <- args[6]
+  weights.file <- args[5]
 
 	## write phenotype to the fam file
 	write_to_fam(path_to_fam_file, path_to_phenotype.filename)
@@ -51,7 +44,7 @@ main <- function() {
 
 	## get mutation weights
 	SNPweights <- Read_SNP_WeightFile(weights.file)
-
+	
 	## get SSD info from created file
 	SSD.info <- Open_SSD(paste(path_to_plink_files, "/file.SSD", sep=""), paste(path_to_plink_files, "/file.info", sep=""))
 
@@ -59,45 +52,17 @@ main <- function() {
 	set.seed(1234)
 	Null_Model <- SKAT_Null_Model(fam_phenotypes_vector ~ 1, out_type="C")
 
-	## perform SKAT on all sets of variants (no weights)
-	set.seed(1234)
-	All_SKAT_Data.no.weights  <- SKAT.SSD.All(SSD.info, weights.beta=c(1,1), Null_Model)
-
-	## sort All_SKAT_Data.no.weights by p-value
-	mydata.SKAT.no.weights <- All_SKAT_Data.no.weights$results
-	p.values.no.weights <- mydata.SKAT.no.weights[order(mydata.SKAT.no.weights[,2]),]
-	#write.table(p.values.no.weights, paste0(path_to_plink_files,"/SKAT_no_weights_results", postfix,".txt"), sep="\t", row.names=FALSE, quote=FALSE, append=FALSE)
-
-	## plot qqplot for no weights analysis
-	pdf(paste0(path_to_plink_files, '/qqplot_no_weights', postfix, '.pdf'), height = 3, width = 3)
-	qq(p.values.no.weights$P.value)
-	dev.off()
-	
-	## do False Discovery Rate analysis for SKAT without weights
-	all_pvals <- p.values.no.weights
-	all_pvals$p_adjust <- p.adjust(all_pvals$P.value,  method = "BH")
-
-	write.table(all_pvals, paste0(path_to_plink_files, "/SKAT_pANDq_no_weights_results", postfix, ".txt"), sep="\t", row.names=FALSE, quote=FALSE, append=FALSE)
-
-	## perform SKAT on all sets of variants with weights
+	## perform SKAT on all sets of variants (use weights)
 	set.seed(1234)
 	All_SKAT_Data  <- SKAT.SSD.All(SSD.info, Null_Model, obj.SNPWeight=SNPweights)
 
 	## sort All_SKAT_Data by p-value
 	mydata.SKAT.weights <- All_SKAT_Data$results
 	p.values.weights <- mydata.SKAT.weights[order(mydata.SKAT.weights[,2]),]
-	#write.table(p.values.weights, paste0(path_to_plink_files, "/SKAT_weights_results", postfix, ".txt"), sep="\t", row.names=FALSE, quote=FALSE, append=FALSE)
 
-	## plot qqplot for SKAT with weights
-	pdf(paste0(path_to_plink_files, '/qqplot_weights', postfix, '.pdf'), height = 3, width = 3)
-	qq(p.values.weights$P.value)
-	dev.off()
-	
-	## do False Discovery Rate analysis for SKAT with weights
-	all_pvals_weights <- p.values.weights
-	all_pvals_weights$p_adjust <- p.adjust(all_pvals_weights$P.value, method = "BH")
-
-	write.table(all_pvals_weights, paste0(path_to_plink_files, "/SKAT_pANDq_weights_results", postfix, ".txt"), sep="\t", row.names=FALSE, quote=FALSE, append=FALSE)
+	## do False Discovery Rate analysis for SKAT without weights
+	p.values.weights$p_adjust <- p.adjust(p.values.weights$P.value, method = "BH")
+	write.table(p.values.weights, paste(path_to_plink_files, "/SKAT_weights.tsv", sep=""), sep="\t", row.names=FALSE, quote=FALSE, append=FALSE)
 
 	## output time to run script
   	the_time <- proc.time() - ptm # Stop the clock
@@ -109,7 +74,7 @@ write_to_fam <- function(path, phenotypes) {
   ##
   ## Arguements:
   ##   path: path to the plink fam file to be added to
-  ##   phenotypes: a file where the first column lists strains and the third column lists
+  ##   phenotypes: a file where the first column lists strains and the second column lists
   ##   phenotype
   ##
   fam.file <- read.table(path)
