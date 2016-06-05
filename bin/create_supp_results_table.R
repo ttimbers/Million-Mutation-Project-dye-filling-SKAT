e3d9d2c2f784faf6657eb54acf54dfdda865acd9
ttimbers <tiffany.timbers@gmail.com>
2016-06-05 10:44:21 -0700

    made writing of supp results csv

--- bin/create_supp_results_table.R
+++ bin/create_supp_results_table.R
@@ -1,9 +1,9 @@
 ## Tiffany Timbers
 ## August 7, 2015
 ##
-## This script takes in the SKAT results from do_SKAT.R and the effect size calculated with 
+## This script takes in the SKAT results from do_SKAT.R and the effect size calculated with
 ## calculate_effect_size.R and creates the supplemental tables for the paper containing columns
-## gene(seq name), p-value, Bonferroni-adjusted-p-value, number of variants, number of dyf strains 
+## gene(seq name), p-value, Bonferroni-adjusted-p-value, number of variants, number of dyf strains
 ## with mutations in that gene, number of non-dyf strains with mutations in that gene
 
 main <- function(){
@@ -16,30 +16,30 @@ main <- function(){
 
   ## Get command line arguments
   args <- commandArgs(trailingOnly = TRUE)
-  SKAT_results_file <- args[1] 
+  SKAT_results_file <- args[1]
   gene_publicName_N_sequenceName <- args[2]
-  outout_table_name <- args[3] 
-  
+  outout_table_name <- args[3]
+
   ## load data files
   SKAT_results <- read.table(SKAT_results_file, header = TRUE)
   gene_publicName_N_sequenceName <- read.table(gene_publicName_N_sequenceName, header=FALSE, sep="\t")
-  
+
   ## drop unnecessary columns
   SKAT_results$N.Marker.All <- NULL
-  
+
   ## rename columns
   colnames(SKAT_results) <- c("sequence", "p-value", "number_of_strains_with_mutations", "Bonferroni_adjusted_p-value")
   colnames(gene_publicName_N_sequenceName) <- c("public_gene_name", "sequence")
-  
+
   ## join tables
   joined_data <- left_join(SKAT_results, gene_publicName_N_sequenceName, by = "sequence")
-  
+
   ## order columns
   joined_data <- joined_data[c("public_gene_name", "sequence", "p-value", "Bonferroni_adjusted_p-value", "number_of_strains_with_mutations")]
-  
+
   ## write out table
-  write.table(joined_data, outout_table_name, row.names = FALSE, quote = FALSE, append = FALSE)
-  
+  write.table(joined_data, outout_table_name, row.names = FALSE, quote = FALSE, append = FALSE, sep = ',')
+
   ## output time to run script
   the_time <- proc.time() - ptm # Stop the clock
   print(paste("It took", the_time[3], "to run create_supp_table.R"))
--- bin/plot_power.R
+++ bin/plot_power.R
@@ -16,74 +16,81 @@
 #           (1 gene, 2 genes, 3 genes, etc.)
 #
 
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(stringr)
-
-input_file <- 'data/SKAT_power_summary_no_path.tsv'
-
-# load the data
-bootstrap_raw <- read.table(input_file, sep = '\t', header = TRUE)
-
-# clean the data (may not always be necessary) to remove extra header rows
-bootstrap_raw <- bootstrap_raw[bootstrap_raw$SetID != "SetID",]
-
-# for each critical value, need to determine the power at each sample size
-# map function to see if there is `critical_value` number of samples with p_adjust < 0.05
-# if so, 1, if no, then 0
-
-# nest/groupby/split by N
-by_N <- bootstrap_raw %>% 
-  group_by(N, ID) %>% 
-  nest() %>% 
-  mutate(one_gene = purrr::map(data, ~ significant(., 'p_adjust'))) %>% 
-  mutate(two_genes = purrr::map(data, ~ significant(., 'p_adjust', 2))) %>% 
-  mutate(three_genes = purrr::map(data, ~ significant(., 'p_adjust', 3))) %>% 
-  mutate(four_genes = purrr::map(data, ~ significant(., 'p_adjust', 4))) %>% 
-  mutate(five_genes = purrr::map(data, ~ significant(., 'p_adjust', 5))) %>% 
-  unnest(one_gene) %>% 
-  unnest(two_genes) %>% 
-  unnest(three_genes) %>% 
-  unnest(four_genes) %>% 
-  unnest(five_genes)
-
-# make long so it can be plotted
-by_N_long <- gather(by_N, gene_n, sig, one_gene:five_genes)
-
-# make gene_n a factor
-by_N_long$gene_n <- as.factor(by_N_long$gene_n)
-
-# get proportions to plot
-power <- by_N_long %>% 
-  group_by(N, gene_n) %>%  
-  summarise(sum(sig)/length(sig))
-
-colnames(power) <- c('N', 'gene_n', 'power')
-
-# make N a number
-power$N <- as.numeric(as.character(power$N))
-
-# change words to numbers in gene_n column
-power$gene_n <- sub('one_gene', '1', power$gene_n)
-power$gene_n <- sub('two_genes', '2', power$gene_n)
-power$gene_n <- sub('three_genes', '3', power$gene_n)
-power$gene_n <- sub('four_genes', '4', power$gene_n)
-power$gene_n <- sub('five_genes', '5', power$gene_n)
-
-# make plot
-power_plot <- ggplot(data = power, aes(x= N, y=power, group=gene_n, colour = gene_n)) +
-  geom_line() +
-  geom_point() +
-  xlab('Sample size') +
-  ylab('Power')
-
-power_plot
-
-
-# save plot
-
-
+main <- function(){
+  
+  # load libraries
+  library(dplyr)
+  library(tidyr)
+  library(ggplot2)
+  library(stringr)
+  
+  # get command line arguments
+  args <- commandArgs(trailingOnly = TRUE)
+  input_file <- args[1] # 'data/SKAT_power_summary_no_path.tsv'
+  output_file <- args[2]
+  
+  # load the data
+  bootstrap_raw <- read.table(input_file, sep = '\t', fill = TRUE, header = TRUE)
+  
+  # clean the data (may not always be necessary) to remove extra header rows
+  bootstrap_raw <- bootstrap_raw[bootstrap_raw$SetID != "SetID",]
+  
+  # for each critical value, need to determine the power at each sample size
+  # map function to see if there is `critical_value` number of samples with p_adjust < 0.05
+  # if so, 1, if no, then 0
+  
+  # nest/groupby/split by N
+  by_N <- bootstrap_raw %>% 
+    group_by(N, ID) %>% 
+    nest() %>% 
+    mutate(one_gene = purrr::map(data, ~ significant(., 'p_adjust'))) %>% 
+    mutate(two_genes = purrr::map(data, ~ significant(., 'p_adjust', 2))) %>% 
+    mutate(three_genes = purrr::map(data, ~ significant(., 'p_adjust', 3))) %>% 
+    mutate(four_genes = purrr::map(data, ~ significant(., 'p_adjust', 4))) %>% 
+    mutate(five_genes = purrr::map(data, ~ significant(., 'p_adjust', 5))) %>% 
+    unnest(one_gene) %>% 
+    unnest(two_genes) %>% 
+    unnest(three_genes) %>% 
+    unnest(four_genes) %>% 
+    unnest(five_genes)
+  
+  # make long so it can be plotted
+  by_N_long <- gather(by_N, gene_n, sig, one_gene:five_genes)
+  
+  # make gene_n a factor
+  by_N_long$gene_n <- as.factor(by_N_long$gene_n)
+  
+  # get proportions to plot
+  power <- by_N_long %>% 
+    group_by(N, gene_n) %>%  
+    summarise(sum(sig)/length(sig))
+  
+  colnames(power) <- c('N', 'gene_n', 'power')
+  
+  # make N a number
+  power$N <- as.numeric(as.character(power$N))
+  
+  # change words to numbers in gene_n column
+  power$gene_n <- sub('one_gene', '1', power$gene_n)
+  power$gene_n <- sub('two_genes', '2', power$gene_n)
+  power$gene_n <- sub('three_genes', '3', power$gene_n)
+  power$gene_n <- sub('four_genes', '4', power$gene_n)
+  power$gene_n <- sub('five_genes', '5', power$gene_n)
+  
+  # make plot
+  power_plot <- ggplot(data = power, aes(x= N, y=power)) +
+    geom_line() +
+    geom_point() +
+    xlab('Sample size') +
+    ylab('Power') +
+    ylim(0, 0.46) +
+    xlim(0,415)
+  
+  power_plot
+  
+  # save plot
+  ggsave(filename = output_file, plot = power_plot, height = 3.5, width = 3.5)
+}
 
 
 # write a function that takes in a data frame and a column name, and returns a 1 if there are any 
@@ -98,3 +105,5 @@ significant <- function(x, column, N = 1, alpha = 0.05) {
     return(0)
   }
 }
+
+main()
